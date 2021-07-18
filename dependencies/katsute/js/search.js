aa33323aa4117b4e5d5d389eda7a5696282cd0f4
Katsute <58778985+Katsute@users.noreply.github.com>
2021-07-18 11:55:32 -0400

    add search

--- dependencies/katsute/js/search.js
+++ dependencies/katsute/js/search.js
@@ -5,29 +5,57 @@ layout: compress
 "use strict";
 
 {
-    const pages = {
-        {% include data/pages.liquid %}
-    };
-
-    const lang = document.documentElement.lang;
+    const lang = document.documentElement.lang.toLowerCase();
 
     const params = Object.fromEntries(new URLSearchParams(window.location.search).entries());
 
-    if(params.q)
-        document.getElementById("search-query").value = params.q;
+    const search_results = document.getElementById("search-results");
 
-    const results = document.getElementById("search-results");
+    if(search_results && params.q){
+        const q = document.getElementById("search-query").value = params.q;
 
-    const index = lunr(() => {
-        this.ref("id");
-        this.field("title", {boost: 10});
-        this.field("content");
-        for(let key in pages)
-            if(key["lang"] == lang)
-                this.add({
-                    "id": key,
-                    "title": pages[key].title,
-                    "content": pages[key].content
-                });
-    });
+        const pages = {
+            {% include data/pages.liquid %}
+        };
+
+        const index = lunr(function (){
+            this.ref("id");
+            this.field("title", {boost: 10});
+            this.field("content");
+            for(let key in pages)
+                if(pages[key].lang == lang)
+                    this.add({
+                        "id": key,
+                        "path": pages[key].path,
+                        "title": pages[key].title,
+                        "content": pages[key].content
+                    });
+        });
+
+        const results = index.search(q);
+        const resultPages = results.map((m) => pages[m.ref]);
+
+        const regexp = new RegExp(q.replace(/[-[\]{}()*+?.,\\^$|#\s]/gm, "\\$&"), "gmi");
+
+        let resultsString = "";
+
+        for(let r of resultPages){
+            let body =
+                r.content.length > 500
+                ? r.content.substring(0, 500) + " …"
+                : r.content;
+
+            resultsString += "<li>";
+            resultsString += r.crumb;
+            resultsString += `<a href="${r.path}">`;
+            resultsString += "<h5>" + r.title.replace(regexp, (s) => `<mark>${s}</mark>`) + "</h5>";
+            resultsString += "</a>";
+            resultsString += "<p>";
+            resultsString += body.replace(regexp, (s) => `<mark>${s}</mark>`);
+            resultsString += "</p>";
+            resultsString += "</li>";
+        }
+
+        search_results.innerHTML = resultsString;
+    }
 }
